85320cb83ea1d31f57b8ecd65ef4dbbf213f7244
ccaycy <91117150+ccaycy@users.noreply.github.com>
2021-09-25 21:26:55 +0800

    Update main.js

--- moe-cdn.caycy.com/assets/js/main.js
+++ moe-cdn.caycy.com/assets/js/main.js
@@ -1,8 +1,9 @@
 var api = "api";
+
 $(document).ready(function () {
     $(".loading").hide();
     getAchives();
-    gethitokoto();
+    getHitokoto();
 });
 
 $('.menu a').click(function () {
@@ -21,7 +22,7 @@ function getAchives() {
     t = ``;
     $.ajax({
         type: "GET",
-        url: api + "wp-json/wp/v2/posts?per_page=10&page=1",
+        url: api + "wp-json/wp/v2/posts?per_page=10&page=1&_fields=date,title,link",
         dataType: "json",
         success: function (json) {
             for (var i = 0; i < json.length; i++) {
@@ -52,10 +53,16 @@ function write(text) {
     if (text.length < 30) {
         $('#hitokoto').html(text);
     } else {
-        gethitokoto();
+        getHitokoto();
     }
 }
 
+// 对Date的扩展，将 Date 转化为指定格式的String
+// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符， 
+// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
+// 例子： 
+// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423 
+// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18 
 Date.prototype.Format = function (fmt) { //author: meizz 
     var o = {
         "M+": this.getMonth() + 1, //月份 
@@ -71,3 +78,28 @@ Date.prototype.Format = function (fmt) { //author: meizz
         if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
 }
+
+//异步加载背景
+
+function blobToDataURI(blob, callback) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        callback(e.target.result);
+    }
+    reader.readAsDataURL(blob);
+}
+var url = "assets/img/bg.jpg";
+var xhr = new XMLHttpRequest();
+xhr.open('GET', url, true);
+xhr.responseType = "blob";
+xhr.onload = function () {
+    if (this.status == 200) {
+        var blob = this.response;
+        blobToDataURI(blob, function (t) {
+            $("body").css("background-image", "url('" + t + "')");
+            $("#background-small").addClass("smallBg");
+            $("#background-small").css("opacity", "0");
+        });
+    }
+}
+xhr.send();
